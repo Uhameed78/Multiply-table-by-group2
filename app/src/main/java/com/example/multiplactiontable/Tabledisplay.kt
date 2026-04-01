38209e230bed0e2d7b7ef97a47992d0886237e5c
Uhameed78 <hameedulla.umar@gmail.com>
2026-04-01 16:53:59 +0200

    Added code and comments on the 2nd page

--- app/src/main/java/com/example/multiplactiontable/Tabledisplay.kt
+++ app/src/main/java/com/example/multiplactiontable/Tabledisplay.kt
@@ -5,12 +5,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
+
 
 class Tabledisplay : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_tabledisplay)
+
+        val bundle: Bundle? = intent.extras
+        // get the table number from the intent
+        val tableString: String? = bundle?.getString("tableNumber")
+        // convert the table number to an integer
+val tableNumber = tableString?.toInt()
+ val multiplyTable = findViewById<TextView>(R.id.tableDistxt)
+        
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)

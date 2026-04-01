5ead8e385bc2ccb5c87125556786a2e94c10e9bb
Uhameed78 <hameedulla.umar@gmail.com>
2026-04-01 16:25:04 +0200

    Added comments

--- app/src/main/java/com/example/multiplactiontable/MainActivity.kt
+++ app/src/main/java/com/example/multiplactiontable/MainActivity.kt
@@ -7,14 +7,18 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.Button
 import android.content.Intent
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
+//Made variables to hold UI
        val button = findViewById<Button>(R.id.multBtn)
+        val editText = findViewById<EditText>(R.id.txtNum)
+
+        //Created click button listener
         button.setOnClickListener {
             // create the explicit intent
             val intent = Intent(this, Tabledisplay::class.java)

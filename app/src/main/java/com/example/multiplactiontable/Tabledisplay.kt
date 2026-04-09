f35381863fffe78f039087cffe873bbb82915f72
Uhameed78 <hameedulla.umar@gmail.com>
2026-04-09 09:24:14 +0200

    added multiply by 10 in code

--- app/src/main/java/com/example/multiplactiontable/Tabledisplay.kt
+++ app/src/main/java/com/example/multiplactiontable/Tabledisplay.kt
@@ -1,5 +1,6 @@
 package com.example.multiplactiontable
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +10,7 @@ import android.widget.TextView
 
 
 class Tabledisplay : AppCompatActivity() {
+    @SuppressLint("SuspiciousIndentation", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,16 +20,28 @@ class Tabledisplay : AppCompatActivity() {
         // get the table number from the intent
         val tableString: String? = bundle?.getString("tableNumber")
         // convert the table number to an integer
-val tableNumber = tableString?.toInt()
+val tableNumber = tableString!!.toInt()
  val multiplyTable = findViewById<TextView>(R.id.tableDistxt)
-        
+  var tableDisplay: String = "$tableNumber x table\n\n"
 
+        multiplyTable.text = tableDisplay
+
+        // declare the control variables
+        var counter = 1
+        while (counter <= 10) { // check the control variable
+            val answer = tableNumber * counter // calculate the answer
+            tableDisplay += "$tableNumber x $counter = ${answer}\n" // add the answer to the table display string
+            counter++ // increment the control variable
+        }
 
 
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+
+
+
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }

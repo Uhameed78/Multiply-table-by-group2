663d3b8db60db20508cbc468041e27683f7fbc9c
Uhameed78 <hameedulla.umar@gmail.com>
2026-04-09 21:37:28 +0200

    changed to 20
    faced issues and bugs

--- app/src/main/java/com/example/multiplactiontable/Tabledisplay.kt
+++ app/src/main/java/com/example/multiplactiontable/Tabledisplay.kt
@@ -1,3 +1,5 @@
+@file:Suppress("RedundantExplicitType")
+
 package com.example.multiplactiontable
 
 import android.annotation.SuppressLint
@@ -19,22 +21,27 @@ class Tabledisplay : AppCompatActivity() {
         val bundle: Bundle? = intent.extras
         // get the table number from the intent
         val tableString: String? = bundle?.getString("tableNumber")
-        // convert the table number to an integer
-val tableNumber = tableString!!.toInt()
- val multiplyTable = findViewById<TextView>(R.id.tableDistxt)
-  var tableDisplay: String = "$tableNumber x table\n\n"
-
-        multiplyTable.text = tableDisplay
+        // convert the table number to an integer safely
+        val tableNumber = tableString?.toIntOrNull() ?: 1
+        val multiplyTable = findViewById<TextView>(R.id.tableDistxt)
+        var tableDisplay: String = "$tableNumber x table\n\n"
 
         // declare the control variables
         var counter = 1
-        while (counter <= 10) { // check the control variable
+        while (counter <= 20) { // check the control variable
+
+            if (counter == 3) {
+                counter++
+                continue
+            }
+
+
             val answer = tableNumber * counter // calculate the answer
             tableDisplay += "$tableNumber x $counter = ${answer}\n" // add the answer to the table display string
             counter++ // increment the control variable
         }
 
-
+        multiplyTable.text = tableDisplay
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
